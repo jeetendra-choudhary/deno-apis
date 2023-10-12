263592e8d65b14a8c0fccc791017f2d667917b7e
Jeetendra Choudhary <jeetendra.choudhary@appswise.com>
2023-10-12 12:02:45 +0530

    Server

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { Application } from './deps.ts'
 import router from './routes/index.ts'
 
 const app = new Application()
+  , port: number = 80
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-// app.addEventListener('listen', ({ secure, hostname, port }) => {
-//   console.log({ secure, hostname, port })
-//   const protocol = secure ? 'https://' : 'http://'
-//     , url = `${protocol}${hostname ?? 'localhost'}:${port}`
-//   console.log({ url })
-//   console.log(`Listening on: ${port}`)
-// })
+app.addEventListener('listen', ({ secure, hostname, port }) => {
+  console.log({ secure, hostname, port })
+  const protocol = secure ? 'https://' : 'http://'
+    , url = `${protocol}${hostname ?? 'localhost'}:${port}`
+  console.log(`Listening on: ${url}`)
+})
 
 await app.listen({ port: 80 });
